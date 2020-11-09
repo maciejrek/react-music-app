e7fcb93b868bb47786497487dfd39d9428acfcc1
Maciej <maciej.rek.kontakt@gmail.com>
2020-11-09 11:20:35 +0100

    Add songs data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./styles/app.scss";
 //components
 import Player from "./components/Player";
 import Song from "./components/Song";
+//import util
+import data from "./util";
 
 function App() {
   return (
