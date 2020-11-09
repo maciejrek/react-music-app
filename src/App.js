fbde3bfc2423c347692924bc481723d6244f5321
Maciej <maciej.rek.kontakt@gmail.com>
2020-11-09 10:15:27 +0100

    Create components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+//components
+import Player from "./components/Player";
+import Song from "./components/Song";
+
 function App() {
   return (
     <div className="App">
-      <h1>Music player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
